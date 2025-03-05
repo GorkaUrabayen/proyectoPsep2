905b60807b43ebcd85af6409cb4985d6ecdac221
GorkaUrabayen <146164587+GorkaUrabayen@users.noreply.github.com>
2025-03-05 15:57:29 +0100

    Revert "Intentando volver ha hacerlo"
    This reverts commit 2fe72dc99e079660b425eca4f6706859c0f5687e.

--- ProyectoPSEP/PokemonApi/Program.cs
+++ ProyectoPSEP/PokemonApi/Program.cs
@@ -1,32 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using PokemonApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuración de los servicios
-builder.Services.AddControllers(); // Necesario para usar los controladores
+builder.Services.AddControllers();
+builder.Services.AddSingleton<CipherService>();
 
-// Configuración de Swagger
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Pokémon API", Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Pokemon API",
+        Version = "v1"
+    });
 });
 
 var app = builder.Build();
 
-// Habilitar la documentación Swagger en desarrollo
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(); // Habilita la generación del archivo Swagger
+    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokémon API v1");
-        c.RoutePrefix = string.Empty;  // Configura Swagger UI para que se cargue en la raíz (http://localhost:5001)
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokemon API v1");
     });
 }
 
-// Configuración de las rutas de los controladores
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
-
-// Ejecutar la aplicación
 app.Run();
